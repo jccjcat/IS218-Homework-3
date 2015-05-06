84da7db6ace836b3eb0595506a4840314e35cfad
Jan Chris Tacbianan <jtacbianan@initiateid.com>
2015-05-06 15:46:51 -0400

    Fixed code for single result queries. Added first query.

--- index.php
+++ index.php
@@ -29,11 +29,13 @@
             }
 
             private function connect() {
-                //$this->connection = new PDO('mysql:host=' . Constants::$SQLHOST . ';dbname=' . Constants::$SQLDB, Constants::$SQLUSER, Constants::$SQLPASS);
+                $this->connection = new PDO('mysql:host=' . Constants::$SQLHOST . ';dbname=' . Constants::$SQLDB, Constants::$SQLUSER, Constants::$SQLPASS);
             }
 
             public function retrieveSingleResult($query) {
-                $result = $this->connection->query($query);
+                $stmt = $this->connection->prepare($query);
+                $stmt->execute();
+                $result = $stmt->fetch();
                 return $result;
             }
 
@@ -49,6 +51,7 @@
              */
             public function __construct() {
                 $this->dbUtil = DatabaseAccess::getInstance();
+                echo '<h2>IS 218 Homework Assignment 3</h2>';
             }
 
             /**
@@ -66,26 +69,29 @@
 
             public function menu() {
                 echo <<< INS
-                    <h2>IS 218 Homework Assignment 3</h2>
                     <p>Click on the Query that you would like to access:</p>
                     <ul>
-                        <li><a href='\\{$_SERVER['PHP_SELF']}?entry=1\\'>Query 1</a></li>
-                        <li><a href='\\{$_SERVER['PHP_SELF']}?entry=2\\'>Query 2</a></li>
-                        <li><a href='\\{$_SERVER['PHP_SELF']}?entry=3\\'>Query 3</a></li>
-                        <li><a href='\\{$_SERVER['PHP_SELF']}?entry=4\\'>Query 4</a></li>
-                        <li><a href='\\{$_SERVER['PHP_SELF']}?entry=5\\'>Query 5</a></li>
-                        <li><a href='\\{$_SERVER['PHP_SELF']}?entry=6\\'>Query 6</a></li>
-                        <li><a href='\\{$_SERVER['PHP_SELF']}?entry=7\\'>Query 7</a></li>
-                        <li><a href='\\{$_SERVER['PHP_SELF']}?entry=8\\'>Query 8</a></li>
-                        <li><a href='\\{$_SERVER['PHP_SELF']}?entry=9\\'>Query 9</a></li>
-                        <li><a href='\\{$_SERVER['PHP_SELF']}?entry=10\\'>Query 10</a></li>
+                        <li><a href='{$_SERVER['PHP_SELF']}?entry=1'>Query 1</a></li>
+                        <li><a href='{$_SERVER['PHP_SELF']}?entry=2'>Query 2</a></li>
+                        <li><a href='{$_SERVER['PHP_SELF']}?entry=3'>Query 3</a></li>
+                        <li><a href='{$_SERVER['PHP_SELF']}?entry=4'>Query 4</a></li>
+                        <li><a href='{$_SERVER['PHP_SELF']}?entry=5'>Query 5</a></li>
+                        <li><a href='{$_SERVER['PHP_SELF']}?entry=6'>Query 6</a></li>
+                        <li><a href='{$_SERVER['PHP_SELF']}?entry=7'>Query 7</a></li>
+                        <li><a href='{$_SERVER['PHP_SELF']}?entry=8'>Query 8</a></li>
+                        <li><a href='{$_SERVER['PHP_SELF']}?entry=9'>Query 9</a></li>
+                        <li><a href='{$_SERVER['PHP_SELF']}?entry=10'>Query 10</a></li>
                     </ul>
 INS;
             }
-            
+
             public function generateResult($num) {
                 switch ($num) {
                     case 1:
+                        $query = 'select first_name, last_name, max(salary) from (select first_name, last_name, salary from employees right join salaries on employees.emp_no=salaries.emp_no) as employeesalaries';
+                        $result = $this->dbUtil->retrieveSingleResult($query);
+                        echo '<h3>Task 1: Who is the highest paid employee?</h3>';
+                        echo '<p> The highest paid is '.$result['first_name'].' '.$result['last_name'].' with a salary of $'.$result['max(salary)'].'</p>';
                         break;
                     case 2:
                         break;
@@ -104,8 +110,9 @@ INS;
                     case 9:
                         break;
                     case 10:
-                        break;                    
+                        break;
                 }
+                echo "<a href='".$_SERVER['PHP_SELF']."'>Return to Menu</a>";
             }
 
         }
@@ -118,6 +125,7 @@ INS;
             public static $SQLDB = 'employees';
 
         }
+
         $page = new Page();
         ?>
     </body>
