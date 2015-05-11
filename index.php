07fffeafe76eea0cf07a75f1cc38540798649a3e
Jan Chris Tacbianan <jtacbianan@initiateid.com>
2015-05-11 05:00:17 -0400

    Added Adding and Updating Employee

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
         <title>
             IS 218 - Homework 3
         </title>
+		<style>
+		table, th, td {
+			border: 1px solid black;
+		}
+		</style>
     </head>
     <body>
         <?php
@@ -45,6 +50,10 @@
                 $result = $stmt->fetchAll();
                 return $result;
             }
+			
+			public function getConnection() {
+				return $this->connection;
+			}
 
         }
 
@@ -69,6 +78,9 @@
 				if(isset($_POST['submit'])) {
 					$this->generateResult(12);
 				}
+				else if(isset($_POST['mod'])) {
+					$this->generateResult(15);
+				}
                 else if (!(isset($_GET["entry"]))) {
                     $this->menu();
                 } else {
@@ -91,6 +103,8 @@
                         <li><a href='{$_SERVER['PHP_SELF']}?entry=8'>Query 8</a></li>
                         <li><a href='{$_SERVER['PHP_SELF']}?entry=9'>Query 9</a></li>
                         <li><a href='{$_SERVER['PHP_SELF']}?entry=10'>Query 10</a></li>
+						<li><a href='{$_SERVER['PHP_SELF']}?entry=11'>Add Employee</a></li>
+						<li><a href='{$_SERVER['PHP_SELF']}?entry=13'>Modify Employee</a></li>
                     </ul>
 INS;
             }
@@ -183,12 +197,95 @@ INS;
 						First Name: <input type="text" name="fname" value=""><br>
 						Last Name:" <input type="text" name="lname" value=""><br>
 						Birth Date: <input type="text" name="bday" value=""><br>						
-						Gender: <input type="text" name="gender" value="Male"><br>
+						Gender: <input type="text" name="gender" value="M"><br>
 						Hire Date: <input type="text" name="hdate" value=""><br>
 						<input type="submit" value="Submit" name="submit">
 						</form> 						
 frm;
+						break;
+					case 12:
+						echo '<h3>Task: Add Employee</h3>';
+						$en=$_POST['empno'];
+						$bd=$_POST['bday']; 
+						$fn=$_POST['fname']; 
+						$ln=$_POST['lname']; 
+						$g=$_POST['gender']; 
+						$hd=$_POST['hdate']; 
+						$query = "insert into employees values(:en, :bd, :fn, :ln, :g, :hd)";
+						$st = $this->dbUtil->getConnection()->prepare($query);
+						$st->execute(array(
+							':en' => $en,
+							':bd' => $bd,
+							':fn' => $fn,
+							':ln' => $ln,
+							':g' =>  $g,
+							':hd' => $hd
+						));
+						if($st->rowCount() == 1) {
+							echo '<h4> Employee '.$en.' Successfully Added.</h4>';
+						} else {
+							echo '<h4> Error Adding Employee.</h4>';
+						}
+						break;
+					case 13:
+						$query = "select * from employees limit 5";
+                        $result = $this->dbUtil->retrieveResults($query);
+						echo '<table>';
+						echo '<tr><td>Employee Number</td><td>First Name</td><td>Last Name</td><td>Birth Date</td><td>Gender</td><td>Hire Date</td><td>Edit</td></tr>';
+						foreach ($result as $row) {
+                            echo "<tr><td>" . $row['emp_no'] . "</td><td>" . $row['first_name'] . "</td><td>" . $row['last_name'] . "</td><td>" . $row['birth_date'] . "</td><td>" . $row['gender'] . "</td><td>" . $row['hire_date'] . "</td><td><a href='{$_SERVER['PHP_SELF']}?entry=14&id=".$row['emp_no']."'>Edit</a></td></tr>";
+						}
+						echo '</table>';
+						break;
+					case 14:
+						$query = "select * from employees where emp_no=".$_GET["id"];
+						$row = $this->dbUtil->retrieveSingleResult($query);
+						echo <<< edt
+						<h3>Task: Edit Employee</h3>
+						<form name="input" action="" method="post">
+						Employee Number: <input type="text" name="empno" value="{$row['emp_no']}" readonly><br>
+						First Name: <input type="text" name="fname" value="{$row['first_name']}"><br>
+						Last Name:" <input type="text" name="lname" value="{$row['last_name']}"><br>
+						Birth Date: <input type="text" name="bday" value="{$row['birth_date']}"><br>						
+						Gender: <input type="text" name="gender" value="{$row['gender']}"><br>
+						Hire Date: <input type="text" name="hdate" value="{$row['hire_date']}"><br>
+						<input type="submit" value="Submit" name="mod">
+						</form> 						
+edt;
+						break;
+					case 15:
+						$en=$_POST['empno'];
+						$bd=$_POST['bday']; 
+						$fn=$_POST['fname']; 
+						$ln=$_POST['lname']; 
+						$g=$_POST['gender']; 
+						$hd=$_POST['hdate']; 
+						$query = "update employees set birth_date=:bd, first_name=:fn, last_name=:ln, gender=:g, hire_date=:hd where emp_no=:en";
+						$st = $this->dbUtil->getConnection()->prepare($query);
+						$st->execute(array(
+							':en' => $en,
+							':bd' => $bd,
+							':fn' => $fn,
+							':ln' => $ln,
+							':g' =>  $g,
+							':hd' => $hd
+						));
+						if($st->rowCount() == 1) {
+							echo '<h4> Employee '.$en.' Successfully Modified.</h4>';
+							$query = "select * from employees where emp_no=".$_POST['empno'];
+							$result = $this->dbUtil->retrieveResults($query);
+							echo '<table>';
+							echo '<tr><td>Employee Number</td><td>First Name</td><td>Last Name</td><td>Birth Date</td><td>Gender</td><td>Hire Date</td><td>Edit</td></tr>';
+							foreach ($result as $row) {
+								echo "<tr><td>" . $row['emp_no'] . "</td><td>" . $row['first_name'] . "</td><td>" . $row['last_name'] . "</td><td>" . $row['birth_date'] . "</td><td>" . $row['gender'] . "</td><td>" . $row['hire_date'] . "</td><td><a href='{$_SERVER['PHP_SELF']}?entry=14&id=".$row['emp_no']."'>Edit</a></td></tr>";
+							}
+						} else {
+							echo '<h4> Error Modifying Employee.</h4>';
+						}
+						
+					break;
                 }
+				
 				
                 echo "<a href='" . $_SERVER['PHP_SELF'] . "'>Return to Menu</a>";
             }
